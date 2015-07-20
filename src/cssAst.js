8c58a6f24227abafc22e4e7e61d6ffed2f138ebf
He Liang <lianghe@thoughtworks.com>
2015-07-20 09:28:29 +0800

    add part of less surpport

--- src/cssAst.js
+++ src/cssAst.js
@@ -1,9 +1,9 @@
-var gonzales = require('gonzales');
+var path = require('path');
+var gonzales = require('gonzales')
+var less = require('less');
 var _ = require('underscore');
 
-var CssTree = function(cssString){
-  var ast = gonzales.srcToCSSP(cssString);
-
+var CssTree = function(source, srcPath) {
   this.selectors = null;
   this.atRulesTree = null;
 
@@ -13,19 +13,33 @@ var CssTree = function(cssString){
   this.specialIds = null;
   this.attrSelectors = null;
 
-  this.initialize(ast);
+  var initializeCssTree = function(css) {
+    var ast = gonzales.srcToCSSP(css.css);
+    this.initialize(ast);
+  };
+
+  var parseError = function(error) {
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
+  };
+
+  less.render(source, {
+    filename: path.resolve(srcPath)
+  }).then(initializeCssTree.bind(this), parseError);
 };
 
 module.exports = CssTree;
 
-CssTree.prototype.initialize = function(ast){
+CssTree.prototype.initialize = function(ast) {
   // Everything that begins with @ in css (@media, @keyframe, etc.)
-  this.atRulesTree = _.filter(ast, function(branch){
+  this.atRulesTree = _.filter(ast, function(branch) {
     return branch[0] === 'atruler' || branch[0] === 's';
   });
 
   // Tree with everything that doesn't start with an @ (classes, elements, ids, etc.)
-  this.selectors = _.filter(ast, function(branch){
+  this.selectors = _.filter(ast, function(branch) {
     return branch[0] !== 'atruler';
   });
 
@@ -34,16 +48,16 @@ CssTree.prototype.initialize = function(ast){
   var css = _.flatten(ast.slice());
   var attrSelectors = [];
 
-  for(var i = 0; i < css.length; i++){
-    if(css[i] === 'clazz'){
+  for (var i = 0; i < css.length; i++) {
+    if (css[i] === 'clazz') {
       classes.push(css[i + 2].toLowerCase());
     }
 
-    if(css[i] === 'attrselector'){
+    if (css[i] === 'attrselector') {
       attrSelectors.push(css[i + 3].toLowerCase());
     }
 
-    if(css[i] === 'shash'){
+    if (css[i] === 'shash') {
       ids.push(css[i + 1].toLowerCase());
     }
   }
@@ -60,22 +74,21 @@ CssTree.prototype.initialize = function(ast){
     return attrselector.replace(/^["|'](.+(?=["|']$))["|']$/, '$1');
   });
 
-  classes.forEach(function(selector){
-    if(selector.match(/^[\w-]+$/)){
+  classes.forEach(function(selector) {
+    if (selector.match(/^[\w-]+$/)) {
       normalClasses.push(selector);
     } else {
       specialClasses.push(selector);
     }
   });
 
-  ids.forEach(function(selector){
-    if(selector.match(/^[\w-]+$/)){
+  ids.forEach(function(selector) {
+    if (selector.match(/^[\w-]+$/)) {
       normalIds.push(selector);
     } else {
       specialIds.push(selector);
     }
   });
-
 
   this.ids = _.uniq(normalIds);
   this.specialIds = _.uniq(specialIds);
@@ -84,16 +97,16 @@ CssTree.prototype.initialize = function(ast){
   this.attrSelectors = attrSelectors;
 };
 
-CssTree.prototype.filterSelectors = function(classes, htmlEls, ids, attrSelectors){
+CssTree.prototype.filterSelectors = function(classes, htmlEls, ids, attrSelectors) {
   var rejectedTwigs = [];
 
-  this.selectors.forEach(function(branch){
-    if(branch[0] !== 'ruleset'){
+  this.selectors.forEach(function(branch) {
+    if (branch[0] !== 'ruleset') {
       return;
     }
 
-    for(var i = 1; i < branch.length; i++){
-      if(branch[i][0] !== 'selector'){
+    for (var i = 1; i < branch.length; i++) {
+      if (branch[i][0] !== 'selector') {
         continue;
       }
 
@@ -103,46 +116,45 @@ CssTree.prototype.filterSelectors = function(classes, htmlEls, ids, attrSelector
     }
   });
 
-  this.selectors =  _.filter(this.selectors, function(branch){
-    if(branch[0] !== 'ruleset'){
+  this.selectors = _.filter(this.selectors, function(branch) {
+    if (branch[0] !== 'ruleset') {
       return true;
     }
 
     var flatBranch = _.flatten(branch);
     return flatBranch.indexOf('simpleselector') > -1;
   });
-
   return rejectedTwigs;
 };
 
-CssTree.prototype.filterAtRules = function(classes, htmlEls, ids, attrSelectors){
+CssTree.prototype.filterAtRules = function(classes, htmlEls, ids, attrSelectors) {
   var rejectedTwigs = [];
 
-  this.atRulesTree.forEach(function(branch){
-    if(branch[0] !== 'atruler'){
+  this.atRulesTree.forEach(function(branch) {
+    if (branch[0] !== 'atruler') {
       return;
     }
 
-    if(_.flatten(branch).indexOf('media') === -1){
+    if (_.flatten(branch).indexOf('media') === -1) {
       return;
     }
 
-    for(var i = 1; i < branch.length; i++){
-      if(branch[i][0] !== 'atrulers'){
+    for (var i = 1; i < branch.length; i++) {
+      if (branch[i][0] !== 'atrulers') {
         continue;
       }
 
-      for(var j = 1; j < branch[i].length; j++){
+      for (var j = 1; j < branch[i].length; j++) {
         var twig = branch[i][j];
 
-        if(twig[0] !== 'ruleset'){
+        if (twig[0] !== 'ruleset') {
           continue;
         }
 
-        for(var k = 0; k < twig.length; k++){
+        for (var k = 0; k < twig.length; k++) {
           var miniTwig = twig[k];
 
-          if(miniTwig[0] !== 'selector'){
+          if (miniTwig[0] !== 'selector') {
             continue;
           }
 
@@ -152,8 +164,8 @@ CssTree.prototype.filterAtRules = function(classes, htmlEls, ids, attrSelectors)
         }
       }
 
-      branch[i] = _.filter(branch[i], function(twig){
-        if(twig[0] !== 'ruleset'){
+      branch[i] = _.filter(branch[i], function(twig) {
+        if (twig[0] !== 'ruleset') {
           return true;
         }
 
@@ -167,17 +179,17 @@ CssTree.prototype.filterAtRules = function(classes, htmlEls, ids, attrSelectors)
   return rejectedTwigs;
 };
 
-CssTree.prototype.removeEmptyAtRules = function(){
-  this.atRulesTree =  _.filter(this.atRulesTree, function(branch){
-    if(branch[0] !== 'atruler'){
+CssTree.prototype.removeEmptyAtRules = function() {
+  this.atRulesTree = _.filter(this.atRulesTree, function(branch) {
+    if (branch[0] !== 'atruler') {
       return true;
     }
 
     var flatBranch = _.flatten(branch);
     var count = 0;
 
-    for(var i = 0; i < flatBranch.length; i++){
-      if(flatBranch[i] === 'property'){
+    for (var i = 0; i < flatBranch.length; i++) {
+      if (flatBranch[i] === 'property') {
         count++;
       }
     }
@@ -186,7 +198,7 @@ CssTree.prototype.removeEmptyAtRules = function(){
   });
 };
 
-var formatCSS = function(styles){
+var formatCSS = function(styles) {
   styles = styles.split('\\n\\n').join('');
   styles = styles.split(' \\n').join('');
   styles = styles.split('   ').join('');
@@ -198,7 +210,7 @@ var formatCSS = function(styles){
   return styles;
 };
 
-CssTree.prototype.toSrc = function(){
+CssTree.prototype.toSrc = function() {
   var classStyles = gonzales.csspToSrc(this.selectors);
   var atStyles = gonzales.csspToSrc(this.atRulesTree);
 
@@ -207,10 +219,10 @@ CssTree.prototype.toSrc = function(){
   return JSON.parse(formatCSS(JSON.stringify(styles)));
 };
 
-var validateId = function(flatTwig, ids){
-  for(var i = 0; i < flatTwig.length; i++){
-    if(flatTwig[i] === 'shash'){
-      if(ids.indexOf(flatTwig[i + 1].toLowerCase()) === -1){
+var validateId = function(flatTwig, ids) {
+  for (var i = 0; i < flatTwig.length; i++) {
+    if (flatTwig[i] === 'shash') {
+      if (ids.indexOf(flatTwig[i + 1].toLowerCase()) === -1) {
         throwDelim = true;
         return false;
       }
@@ -219,10 +231,10 @@ var validateId = function(flatTwig, ids){
   return true;
 };
 
-var validateClass = function(flatTwig, classes){
-  for(var i = 1; i < flatTwig.length; i++){
-    if(flatTwig[i] === 'clazz'){
-      if(classes.indexOf(flatTwig[i + 2].toLowerCase()) === -1){
+var validateClass = function(flatTwig, classes) {
+  for (var i = 1; i < flatTwig.length; i++) {
+    if (flatTwig[i] === 'clazz') {
+      if (classes.indexOf(flatTwig[i + 2].toLowerCase()) === -1) {
         return false;
       }
     }
@@ -230,41 +242,41 @@ var validateClass = function(flatTwig, classes){
   return true;
 };
 
-var validateHtmlTag = function(flatTwig, htmlEls){
-  for(var i = 1; i < flatTwig.length; i++){
-    if(flatTwig[i] === 'ident' && htmlEls.indexOf(flatTwig[i + 1]) > -1){
+var validateHtmlTag = function(flatTwig, htmlEls) {
+  for (var i = 1; i < flatTwig.length; i++) {
+    if (flatTwig[i] === 'ident' && htmlEls.indexOf(flatTwig[i + 1]) > -1) {
       return true;
     }
   }
   return false;
 };
 
-var filterSelector = function(branch, classes, htmlEls, ids, attrSelectors){
+var filterSelector = function(branch, classes, htmlEls, ids, attrSelectors) {
   var throwDelim = false;
   var rejectedTwigs = [];
 
-  var output = _.filter(branch, function(twig){
+  var output = _.filter(branch, function(twig) {
     var flatTwig = _.flatten(twig);
 
-    if(flatTwig[0] === 'delim' && throwDelim){
+    if (flatTwig[0] === 'delim' && throwDelim) {
       throwDelim = false;
       return false;
     }
 
-    if(flatTwig[0] !== 'simpleselector'){
+    if (flatTwig[0] !== 'simpleselector') {
       return true;
     }
 
-    if(flatTwig.indexOf('pseudoe') > -1 || flatTwig.indexOf('*') > -1 || flatTwig.indexOf('pseudoc') > -1){
+    if (flatTwig.indexOf('pseudoe') > -1 || flatTwig.indexOf('*') > -1 || flatTwig.indexOf('pseudoc') > -1) {
       return true;
     }
 
     var isAttrSelector = flatTwig.indexOf('attrselector') > -1;
-    if(isAttrSelector){
-      for(var k = 0; k < flatTwig.length; k++){
-        if(flatTwig[k] === 'attrselector'){
+    if (isAttrSelector) {
+      for (var k = 0; k < flatTwig.length; k++) {
+        if (flatTwig[k] === 'attrselector') {
           var attrvalue = flatTwig[k + 3].toLowerCase().replace(/^["|'](.+(?=["|']$))["|']$/, '$1');
-          if(attrSelectors.indexOf(attrvalue) === -1){
+          if (attrSelectors.indexOf(attrvalue) === -1) {
             throwDelim = true;
             return false;
           }
@@ -274,27 +286,27 @@ var filterSelector = function(branch, classes, htmlEls, ids, attrSelectors){
     }
 
     var isId = flatTwig.indexOf('shash') > -1;
-    if(isId){
+    if (isId) {
       var validated = validateId(flatTwig, ids);
       throwDelim = !validated;
-      if(!validated){
+      if (!validated) {
         rejectedTwigs.push(twig);
       }
       return validated;
     }
 
     var hasClass = flatTwig.indexOf('clazz') > -1;
-    if(hasClass){
+    if (hasClass) {
       var validated = validateClass(flatTwig, classes);
       throwDelim = !validated;
-      if(!validated){
+      if (!validated) {
         rejectedTwigs.push(twig);
       }
       return validated;
     } else {
       var validated = validateHtmlTag(flatTwig, htmlEls);
       throwDelim = !validated;
-      if(!validated){
+      if (!validated) {
         rejectedTwigs.push(twig);
       }
       return validated;
@@ -303,7 +315,7 @@ var filterSelector = function(branch, classes, htmlEls, ids, attrSelectors){
     return true;
   });
 
-  if(output[output.length - 1][0] === 'delim'){
+  if (output[output.length - 1][0] === 'delim') {
     output = output.slice(0, output.length - 1);
   }
 
--- src/purifycss.js
+++ src/purifycss.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var _ = require('underscore');
 var CleanCss = require('clean-css');
-var CssSyntaxTree = require('./cssTree.js');
+var CssSyntaxTree = require('./cssAst.js');
 var Extraction = require('./extraction.js');
 
 ////////////////////
@@ -22,8 +22,8 @@ var DEFAULT_OPTIONS = {
   info: false
 };
 
-var purify = function(searchThrough, css, options, callback){
-  if(typeof options === 'function'){
+var purify = function(searchThrough, css, options, callback) {
+  if (typeof options === 'function') {
     callback = options;
     options = {};
   }
@@ -39,103 +39,105 @@ var purify = function(searchThrough, css, options, callback){
   var startTime = new Date();
 
   // Turn css into abstract syntax tree
-  var tree = new CssSyntaxTree(cssString);
+  var tree = new CssSyntaxTree(cssString, css[0]);
 
   // Narrow list down to things that are found in content
-  var extraction = new Extraction(content);
-  var classes = extraction.filter(tree.classes);
-  var specialClasses = extraction.filterBySearch(tree.specialClasses);
-  var ids = extraction.filter(tree.ids);
-  var specialIds = extraction.filterBySearch(tree.specialIds);
-  var attrSelectors = extraction.filterBySearch(tree.attrSelectors);
+  setTimeout(function() {
+    var extraction = new Extraction(content);
+    var classes = extraction.filter(tree.classes);
+    var specialClasses = extraction.filterBySearch(tree.specialClasses);
+    var ids = extraction.filter(tree.ids);
+    var specialIds = extraction.filterBySearch(tree.specialIds);
+    var attrSelectors = extraction.filterBySearch(tree.attrSelectors);
 
-  classes = classes.concat(specialClasses);
-  ids = ids.concat(specialIds);
-  var usedHtmlEls = extraction.filter(htmlEls);
+    classes = classes.concat(specialClasses);
+    ids = ids.concat(specialIds);
+    var usedHtmlEls = extraction.filter(htmlEls);
 
-  // Narrow CSS tree down to things that remain on the list
-  var rejectedSelectorTwigs = tree.filterSelectors(classes, usedHtmlEls, ids, attrSelectors);
-  var rejectedAtRuleTwigs = tree.filterAtRules(classes, usedHtmlEls, ids, attrSelectors);
+    // Narrow CSS tree down to things that remain on the list
+    var rejectedSelectorTwigs = tree.filterSelectors(classes, usedHtmlEls, ids, attrSelectors);
+    var rejectedAtRuleTwigs = tree.filterAtRules(classes, usedHtmlEls, ids, attrSelectors);
 
-  // Turn tree back into css
-  var source = tree.toSrc();
+    // Turn tree back into css
+    var source = tree.toSrc();
 
-  if(options.minify){
-    source = new CleanCss().minify(source).styles;
-  }
+    if (options.minify) {
+      source = new CleanCss().minify(source).styles;
+    }
 
-  if(options.info){
-    printInfo(startTime, beginningLength, source.length);
-  }
+    if (options.info) {
+      printInfo(startTime, beginningLength, source.length);
+    }
 
-  if(options.rejected){
-    printRejected(rejectedSelectorTwigs.concat(rejectedAtRuleTwigs));
-  }
+    if (options.rejected) {
+      printRejected(rejectedSelectorTwigs.concat(rejectedAtRuleTwigs));
+    }
 
-  if(!options.output){
-    return callback ? callback(source) : source;
-  } else {
-    fs.writeFile(options.output, source, function(err){
-      if(err) return err;
-    });
-  }
+    if (!options.output) {
+      return callback ? callback(source) : source;
+    } else {
+      fs.writeFile(options.output, source, function(err) {
+        if (err) return err;
+      });
+    }
+  }, 1000);
 };
 
 module.exports = purify;
 
-var concatFiles = function(files){
-  return files.reduce(function(total, file){
+var concatFiles = function(files) {
+  return files.reduce(function(total, file) {
     return total + fs.readFileSync(file, 'utf8') + ' ';
   }, '');
 };
 
-var reduceContent = function(content){
+var reduceContent = function(content) {
   return content
     .split('\n').join('')
     .replace(/\s\s+/g, ' ');
 };
 
-var getRuleString = function(twig){
+var getRuleString = function(twig) {
   var ruleString = '';
-  for(var i = 1; i < twig.length; i++){
+  for (var i = 1; i < twig.length; i++) {
     var rulePart = twig[i];
     switch (rulePart[0]) {
-    case 's':
-      ruleString += rulePart[1] !== '\n' ? rulePart[1] : '';
-      break;
-    case 'clazz':
-      ruleString += '.' + rulePart[1][1];
-      break;
-    case 'shash':
-      ruleString += '#' + rulePart[1];
-      break;
-    case 'ident':
-      ruleString += rulePart[1];
-      break;
-    case 'attrib':
-      ruleString += '[' + rulePart[1][1] + ']';
-      break;
-    default:
-      ruleString += 'Unsupported: ' + JSON.stringify(twig);
+      case 's':
+        ruleString += rulePart[1] !== '\n' ? rulePart[1] : '';
+        break;
+      case 'clazz':
+        ruleString += '.' + rulePart[1][1];
+        break;
+      case 'shash':
+        ruleString += '#' + rulePart[1];
+        break;
+      case 'ident':
+        ruleString += rulePart[1];
+        break;
+      case 'attrib':
+        ruleString += '[' + rulePart[1][1] + ']';
+        break;
+      default:
+        ruleString += 'Unsupported: ' + JSON.stringify(twig);
     }
   }
   return ruleString;
 };
 
-var printInfo = function(startTime, beginningLength, endingLength){
+var printInfo = function(startTime, beginningLength, endingLength) {
   console.log('##################################');
   console.log('Before purify, CSS was ' + beginningLength + ' chars long.');
   console.log('After purify, CSS is ' + endingLength + ' chars long. (' +
-    Math.floor((beginningLength / endingLength * 10)) / 10  + ' times smaller)');
+    Math.floor((beginningLength / endingLength * 10)) / 10 + ' times smaller)');
   console.log('##################################');
   console.log('This function took: ', new Date() - startTime, 'ms');
 };
 
-var printRejected = function(rejectedTwigs){
+var printRejected = function(rejectedTwigs) {
   console.log('##################################');
   console.log('Rejected selectors:');
   console.log(_.map(rejectedTwigs, getRuleString).join('\n'));
   console.log('##################################');
 }
 
-var htmlEls = ['h1','h2','h3','h4','h5','h6','a','abbr','acronym','address','applet','area','article','aside','audio','b','base','basefont','bdi','bdo','bgsound','big','blink','blockquote','body','br','button','canvas','caption','center','cite','code','col','colgroup','command','content','data','datalist','dd','del','details','dfn','dialog','dir','div','dl','dt','element','em','embed','fieldset','figcaption','figure','font','footer','form','frame','frameset','head','header','hgroup','hr','html','i','iframe','image','img','input','ins','isindex','kbd','keygen','label','legend','li','link','listing','main','map','mark','marquee','menu','menuitem','meta','meter','multicol','nav','nobr','noembed','noframes','noscript','object','ol','optgroup','option','output','p','param','picture','plaintext','pre','progress','q','rp','rt','rtc','ruby','s','samp','script','section','select','shadow','small','source','spacer','span','strike','strong','style','sub','summary','sup','table','tbody','td','template','textarea','tfoot','th','thead','time','title','tr','track','tt','u','ul','var','video','wbr','xmp'];
+var htmlEls = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'bgsound', 'big', 'blink', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'command', 'content', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'element', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'image', 'img', 'input', 'ins', 'isindex', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'listing', 'main', 'map', 'mark', 'marquee', 'menu', 'menuitem', 'meta', 'meter', 'multicol', 'nav', 'nobr', 'noembed', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'plaintext', 'pre', 'progress', 'q', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'shadow', 'small', 'source', 'spacer', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr', 'xmp'];
